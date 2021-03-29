67258830a715488e24d6042aeb8d71f8b73e1542
vkSo228 <81561155+vkSo228@users.noreply.github.com>
2021-03-29 15:09:44 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1617018633,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/Новыйпроект-sheet0.png",
+		"images/Новыйпроект2-sheet0.png",
+		"images/Новыйпроект3-sheet0.png",
+		"images/v-sheet0.png",
+		"images/Новыйпроект4-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
